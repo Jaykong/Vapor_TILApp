b48a05993597bb3524bc4571e4826b8a513cbddf
Jay Kong <jay.kong@ef.com>
2018-06-22 15:01:37 +0800

    first commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,8 @@ import Vapor
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentSQLiteProvider())
-
+    let myService = NIOServerConfig.default(port: 8002)
+    services.register(myService)
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -27,7 +28,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .sqlite)
     services.register(migrations)
 
 }
